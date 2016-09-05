1dfaa806d18eed3b6a177ee075323ed1e971858d
Teodor Niżyński <teodornizynski@pc26.home>
2016-09-05 02:04:08 +0200

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Teodor Niżyński on 05.09.2016.
+//  Copyright © 2016 Teodor Niżyński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
