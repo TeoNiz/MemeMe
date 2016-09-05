87d6bd34c623a76ddac83a7f96fe27f5134e2fab
Teodor Niżyński <teodornizynski@pc26.home>
2016-09-05 02:37:58 +0200

    First commit - simple picking images from lessons

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func pickImage(sender: AnyObject){
+        let imagePicker=UIImagePickerController()
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
 
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        print("We picked something!")
+        print(info.description)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker:UIImagePickerController){
+        print("Och noes pick was canceled!")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
